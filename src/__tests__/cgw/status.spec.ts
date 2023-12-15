81ba65726b4a27863a9d0983d49192d514cf238c
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2023-12-16 00:03:15 +0100

    Rename tests

--- src/__tests__/cgw/status.spec.ts
+++ src/__tests__/cgw/status.spec.ts
@@ -2,25 +2,29 @@ import { httpClient } from '@/datasources/axios-http-client';
 import { expect } from '@jest/globals';
 import '@/__tests__/matchers/to-be-string-or-null';
 
-describe('Math functions', () => {
+describe('CGW status checks', () => {
   const cgwBaseUri = 'https://safe-client.staging.5afe.dev';
 
-  it('/health/live should return OK', async () => {
-    const { data } = await httpClient.get(`${cgwBaseUri}/health/live`);
-    expect(data).toEqual({ status: 'OK' });
+  describe('/health endpoints', () => {
+    it('should return OK to liveness probe', async () => {
+      const { data } = await httpClient.get(`${cgwBaseUri}/health/live`);
+      expect(data).toEqual({ status: 'OK' });
+    });
+
+    it('should return OK to readiness probe', async () => {
+      const { data } = await httpClient.get(`${cgwBaseUri}/health/ready`);
+      expect(data).toEqual({ status: 'OK' });
+    });
   });
 
-  it('/health/ready should return OK', async () => {
-    const { data } = await httpClient.get(`${cgwBaseUri}/health/ready`);
-    expect(data).toEqual({ status: 'OK' });
-  });
-
-  it('/about should return the service metadata', async () => {
-    const { data } = await httpClient.get(`${cgwBaseUri}/about`);
-    expect(data).toMatchObject({
-      name: expect.any(String),
-      version: expect.anyStringOrNull(),
-      buildNumber: expect.anyStringOrNull(),
+  describe('/about endpoint', () => {
+    it('should return the service metadata', async () => {
+      const { data } = await httpClient.get(`${cgwBaseUri}/about`);
+      expect(data).toMatchObject({
+        name: expect.any(String),
+        version: expect.anyStringOrNull(),
+        buildNumber: expect.anyStringOrNull(),
+      });
     });
   });
 });
